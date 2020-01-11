2f112efdf3877f0600fe570387d255e5ce4458ed
gulshan kumar <gulhan@123>
2020-01-11 12:04:09 +0530

    Removed ghostSnake from main

--- snake.js
+++ snake.js
@@ -55,17 +55,17 @@ class Snake {
 const NUM_OF_COLS = 100;
 const NUM_OF_ROWS = 60;
 
-const GRID_ID = 'grid';
+const GRID_ID = "grid";
 
 const getGrid = () => document.getElementById(GRID_ID);
-const getCellId = (colId, rowId) => colId + '_' + rowId;
+const getCellId = (colId, rowId) => colId + "_" + rowId;
 
 const getCell = (colId, rowId) =>
   document.getElementById(getCellId(colId, rowId));
 
 const createCell = function(grid, colId, rowId) {
-  const cell = document.createElement('div');
-  cell.className = 'cell';
+  const cell = document.createElement("div");
+  cell.className = "cell";
   cell.id = getCellId(colId, rowId);
   grid.appendChild(cell);
 };
@@ -114,33 +114,13 @@ const main = function() {
       [42, 25]
     ],
     new Direction(EAST),
-    'snake'
+    "snake"
   );
-
-  const ghostSnake = new Snake(
-    [
-      [40, 30],
-      [41, 30],
-      [42, 30]
-    ],
-    new Direction(SOUTH),
-    'ghost'
-  );
-
   attachEventListeners(snake);
   createGrids();
   drawSnake(snake);
-  drawSnake(ghostSnake);
 
   setInterval(() => {
     moveAndDrawSnake(snake);
-    moveAndDrawSnake(ghostSnake);
   }, 200);
-
-  setInterval(() => {
-    let x = Math.random() * 100;
-    if (x > 50) {
-      ghostSnake.turnLeft();
-    }
-  }, 500);
 };
